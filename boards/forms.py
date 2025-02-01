ccc5a6e0d3227e30170a9d6e6bcad0395d75e4b4
kirisame19 <kirisamekari19@gmail.com>
2025-02-01 13:35:53 +0900

    掲示板サンプルの作成ーー関数ベースビューの演習

---
+++ boards/forms.py
@@ -0,0 +1,23 @@
+﻿from django import forms
+from .models import Themes,Comments
+
+class CreateThemeForm(forms.ModelForm):
+    title = forms.CharField(label="タイトル")
+
+    class Meta:
+        model = Themes
+        fields = ("title",)
+
+class DeleteThemeForm(forms.ModelForm):
+    
+    class Meta:
+        model = Themes
+        fields = []
+
+class PostCommentForm(forms.ModelForm):
+    # rows:行数     cols:列数
+    comment = forms.CharField(label="",widget=forms.Textarea(attrs={'rows':5,"cols":'60'}))
+
+    class Meta:
+        model = Comments
+        fields = ("comment",)
